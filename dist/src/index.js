7a6cefa07b0b3d4407904a4ca779a0e32554744c
Danielzxccc <araojodaniel321@gmail.com>
2023-05-16 02:50:34 +0800

    session store in postgres

--- dist/src/index.js
+++ dist/src/index.js
@@ -31,26 +31,28 @@ const dotenv = __importStar(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const express_session_1 = __importDefault(require("express-session"));
-const memorystore_1 = __importDefault(require("memorystore"));
+const connect_session_knex_1 = __importDefault(require("connect-session-knex"));
+// import MemoryStore from 'memorystore'
 dotenv.config();
 const payment_router_1 = require("./payment/payment.router");
 const reservation_router_1 = require("./reservation/reservation.router");
 const auth_router_1 = require("./auth/auth.router");
 const cors_2 = require("../config/cors");
+const db_1 = require("../config/db");
 const PORT = 8080;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)(cors_2.corsOptions));
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
-app.set('trust proxy', 1);
-const store = (0, memorystore_1.default)(express_session_1.default);
+const store = new ((0, connect_session_knex_1.default)(express_session_1.default))({
+    knex: db_1.client,
+    tablename: 'sessions', // optional, defaults to 'sessions'
+});
 app.use((0, express_session_1.default)({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-    store: new store({
-        checkPeriod: 86400000, // prune expired entries every 24h
-    }),
+    store: store,
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,
     },
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
-import MemoryStore from 'memorystore'
+import KnexSessionStore from 'connect-session-knex'
+// import MemoryStore from 'memorystore'
 dotenv.config()
 
 import { paymentRouter } from './payment/payment.router'
 import { reservationRouter } from './reservation/reservation.router'
 import { authRouter } from './auth/auth.router'
 import { corsOptions } from '../config/cors'
+import { client } from '../config/db'
 const PORT: number = 8080
 
 const app = express()
@@ -17,17 +19,21 @@ app.use(cors(corsOptions))
 app.use(cookieParser())
 app.use(express.json())
 
-app.set('trust proxy', 1)
-const store = MemoryStore(session)
+interface Store extends session.Store {
+  // add any additional properties or methods you need here
+}
+
+const store: Store = new (KnexSessionStore(session))({
+  knex: client,
+  tablename: 'sessions', // optional, defaults to 'sessions'
+})
 
 app.use(
   session({
     secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-    store: new store({
-      checkPeriod: 86400000, // prune expired entries every 24h
-    }),
+    store: store,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
     },
