9c32f493a5b23e79723b9c14067d069b82083eff
Danielzxccc <araojodaniel321@gmail.com>
2023-05-23 15:22:13 +0800

    test jwt functions

--- src/reservation/reservation.router.ts
+++ src/reservation/reservation.router.ts
@@ -24,20 +24,16 @@ reservationRouter.post('/reserve', async (req: Request, res: Response) => {
   }
 })
 
-reservationRouter.get(
-  '/get/:id',
-  verifyJWT,
-  async (req: Request, res: Response) => {
-    try {
-      const { id } = req.params
-      const { date } = req.query
-      const getReservations = await check(id as string, date as string)
-      res.status(200).json(getReservations)
-    } catch (error) {
-      res.status(500).json({
-        error: true,
-        message: error.message,
-      })
-    }
+reservationRouter.get('/get/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const { date } = req.query
+    const getReservations = await check(id as string, date as string)
+    res.status(200).json(getReservations)
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: error.message,
+    })
   }
-)
+})
